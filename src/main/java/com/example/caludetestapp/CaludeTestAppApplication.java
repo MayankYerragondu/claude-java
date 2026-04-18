0081f4ad9178534eaac43ba934d51e1abe73cd6f
Mayank Yerragondu <myerragondu@gmail.com>
2026-04-18 10:00:03 -0230

    add App main

---
+++ src/main/java/com/example/caludetestapp/CaludeTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.caludetestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaludeTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaludeTestAppApplication.class, args);
+    }
+
+}
