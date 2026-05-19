df3b397d725d79ab9de49bab98343e1b78ede28b
Mayank Yerragondu <myerragondu@gmail.com>
2026-05-19 16:35:24 -0400

    Create OpenApiConfig.java

---
+++ src/main/java/com/example/caludetestapp/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.example.caludetestapp.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CaludeTestApp API")
+                        .description("REST API documentation for CaludeTestApp")
+                        .version("0.0.1-SNAPSHOT")
+                        .contact(new Contact()
+                                .name("CaludeTestApp Team")));
+    }
+}
